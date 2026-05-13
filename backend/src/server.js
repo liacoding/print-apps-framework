a3d2407f5eaa79ea0d961efa7b85516bf0d5a444
Olga N <nazar.olgaa@gmail.com>
2026-05-13 15:38:36 -0400

    Deployment config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,25 @@
 import express from "express";
+import path from "path";
+import { ENV_CONFIG } from "./config/envConfig.js";
 
 const app = express();
 
-app.get("/", (req, res) => {
+const __dirname = path.resolve();
+
+app.get("/api/health", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Serve static files 
+
+if(ENV_CONFIG.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  })
+} 
+
+app.listen(ENV_CONFIG.PORT, () => {
+  console.log(`Server is running on port ${ENV_CONFIG.PORT}`);
 })
